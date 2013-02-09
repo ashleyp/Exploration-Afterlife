40b4cc161426d95c2af8032244d23a8850d70133
ashpe <irashp@gmail.com>
2013-02-09 13:01:38 +1100

    Update style. Apply bootstrap!

--- lib/Exploration/Afterlife.pm
+++ lib/Exploration/Afterlife.pm
@@ -7,7 +7,7 @@ our $VERSION = '0.1';
 
 get '/' => sub {
     my @articles = schema->resultset('Article')->all;
-    template 'index', { articles => \@articles };
+    template index => { articles => \@articles };
 };
 
 get '/add_user' => sub {
@@ -15,15 +15,17 @@ get '/add_user' => sub {
 };
 
 post '/add_user' => sub {
-    # TODO: validate all these fields are what they should be. 
-    my $add_article = schema->resultset('Users')->create({
-        user_level_id     => 1,
-        username          => param('username'),
-        password          => param('password'),
-        firstname         => param('firstname'),
-        lastname          => param('lastname'),
-        email             => param('email'),
-    });
+    # TODO: validate all these fields are what they should be.
+    my $add_article = schema->resultset('Users')->create(
+        {
+            user_level_id => 1,
+            username      => param('username'),
+            password      => param('password'),
+            firstname     => param('firstname'),
+            lastname      => param('lastname'),
+            email         => param('email'),
+        }
+    );
 };
 
 get '/login' => sub {
@@ -37,66 +39,77 @@ get '/logout' => sub {
 };
 
 get '/evidence' => sub {
-    my @categories  = schema->resultset('Category')->all;
-    my @articles    = schema->resultset('Article')->all;
-    template 'evidence', {
-        articles => \@articles,
+    my @categories = schema->resultset('Category')->all;
+    my @articles   = schema->resultset('Article')->all;
+    template 'evidence',
+      {
+        articles   => \@articles,
         categories => \@categories,
-    };
+      };
 };
 
 get '/evidence/category/:category' => sub {
-    my @categories      = schema->resultset('Category')->all;
-    my $find_category   = schema->resultset('Category')->find({ 
-            category_name => param('category') 
-    });
+    my @categories = schema->resultset('Category')->all;
+    my $find_category
+      = schema->resultset('Category')->find( { category_name => param('category') } );
     my @articles_for_category = $find_category->articles;
-    template 'evidence', {
-        articles => \@articles_for_category,
+    template 'evidence',
+      {
+        articles   => \@articles_for_category,
         categories => \@categories,
-    };
+      };
 };
 
 get '/add_article' => sub {
-    my $user_data   = authd;
-    my @categories  = schema->resultset('Category')->all;
-    template 'add_article', { categories => \@categories };
+    my $user_data  = authd;
+    my @categories = schema->resultset('Category')->all;
+    template add_article => { categories => \@categories };
 };
 
 post '/add_article' => sub {
     my $user_data   = authd;
-    my $add_article = schema->resultset('Article')->create({
+    my $add_article = schema->resultset('Article')->create(
+        {
             content           => param('content'),
             title             => param('title'),
             category_id       => param('category'),
             date_posted       => time(),
             posted_by_user_id => $user_data->{id},
-    });
+        }
+    );
     redirect '/';
 };
 
 
 post '/login' => sub {
-    my $user_ok  = auth( param('username'), param('password') );
-    if ( !$user_ok->errors ) {
+    my $user_ok = auth( param('username'), param('password') );
+    if( !$user_ok->errors ) {
         redirect param('path') || '/';
-    } else {
+    }
+    else {
         var requested_path => param('path');
         redirect '/login?error=true';
     }
 };
 
 get '/edit_article/:article_id' => sub {
-    my $user_data   = authd;
-    my @categories  = schema->resultset('Category')->all;
-    my $article     = schema->resultset('Article')->find({
-            article_id => param('article_id'),
-    });
-    template 'edit_article', { article => $article, categories => \@categories };
+    my $user_data  = authd;
+    my @categories = schema->resultset('Category')->all;
+    my $article    = schema->resultset('Article')->find({ article_id => param('article_id') });
+    template edit_article => { article => $article, categories => \@categories };
 };
 
 post '/edit_article/:article_id' => sub {
-    print "Testing\n\n\n\n";
+    my $user_data  = authd;
+    my $article    = schema->resultset('Article')->find( { article_id => param('article_id'), } );
+    $article->update(
+        {
+            content     => param('content'),
+            title       => param('title'),
+            category_id => param('category'),
+        }
+    );
+    redirect '/evidence';
 };
 
 true;
